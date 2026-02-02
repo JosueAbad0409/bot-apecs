d78947fa41b7945ac6894869cd3bcc04fd004fd5
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-02-02 17:50:19 -0500

    Update GeminiService.java

--- ApecsChatBot/src/main/java/ista/M3A/service/GeminiService.java
+++ ApecsChatBot/src/main/java/ista/M3A/service/GeminiService.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class GeminiService {
